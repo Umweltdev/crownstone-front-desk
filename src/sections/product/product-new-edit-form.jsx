9641f86ad44c3a20f38fc20526af678f1de5123f
Sugarcothe <ifeanyivalentine82@gmail.com>
2025-06-19 08:49:39 +0100

    init commit

---
+++ src/sections/product/product-new-edit-form.jsx
@@ -0,0 +1,435 @@
+import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMemo, useState, useEffect, useCallback } from 'react';
+
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Switch from '@mui/material/Switch';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Unstable_Grid2';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import LoadingButton from '@mui/lab/LoadingButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+import { paths } from 'src/routes/paths';
+import { useRouter } from 'src/routes/hooks';
+
+import { useResponsive } from 'src/hooks/use-responsive';
+
+import axiosInstance from 'src/utils/axios';
+
+import {
+  _tags,
+  PRODUCT_SIZE_OPTIONS,
+  PRODUCT_GENDER_OPTIONS,
+  PRODUCT_COLOR_NAME_OPTIONS,
+  PRODUCT_CATEGORY_GROUP_OPTIONS,
+} from 'src/_mock';
+
+import { useSnackbar } from 'src/components/snackbar';
+import FormProvider, {
+  RHFSelect,
+  RHFEditor,
+  RHFUpload,
+  RHFSwitch,
+  RHFTextField,
+  RHFMultiSelect,
+  RHFAutocomplete,
+  RHFMultiCheckbox,
+} from 'src/components/hook-form';
+
+// ----------------------------------------------------------------------
+
+export default function ProductNewEditForm({ currentProduct }) {
+  const router = useRouter();
+
+  const mdUp = useResponsive('up', 'md');
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [includeTaxes, setIncludeTaxes] = useState(false);
+
+  const NewProductSchema = Yup.object().shape({
+    title: Yup.string().required('Title is required'),
+    description: Yup.string().required('Description is required'),
+    content: Yup.string().required('Content is required'),
+    image: Yup.mixed().nullable().required('Cover is required'),
+    venue: Yup.string().required('Venue is required'),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      title: currentProduct?.title || '',
+      description: currentProduct?.description || '',
+      content: currentProduct?.content || '',
+      image: currentProduct?.image || null,
+      publish: currentProduct?.publish === 'published' || false,
+      venue: currentProduct?.venue || '',
+    }),
+    [currentProduct]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(NewProductSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  useEffect(() => {
+    if (currentProduct) {
+      reset(defaultValues);
+    }
+  }, [currentProduct, defaultValues, reset]);
+
+  useEffect(() => {
+    if (includeTaxes) {
+      setValue('taxes', 0);
+    } else {
+      setValue('taxes', currentProduct?.taxes || 0);
+    }
+  }, [currentProduct?.taxes, includeTaxes, setValue]);
+
+  const onSubmit = handleSubmit(async (data) => {
+    const transformedData = {
+      ...data,
+      publish: data.publish ? 'published' : 'draft', // Convert boolean to string
+    };
+    const formData = new FormData();
+    Object.keys(transformedData).forEach((key) => {
+      formData.append(key, transformedData[key]);
+    });
+    try {
+      if (currentProduct) {
+        await axiosInstance.put(`/api/event/${currentProduct._id}`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+      } else {
+        await axiosInstance.post(`/api/event`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+      }
+      reset();
+      enqueueSnackbar(currentProduct ? 'Update success!' : 'Create success!');
+      router.push(paths.dashboard.event.root);
+      // console.info('DATA', data);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+      console.log(newFile);
+      if (file) {
+        setValue('image', newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  const handleRemoveFile = useCallback(() => {
+    setValue('image', null);
+  }, [setValue]);
+
+  const handleChangeIncludeTaxes = useCallback((event) => {
+    setIncludeTaxes(event.target.checked);
+  }, []);
+
+  const renderDetails = (
+    <>
+      {mdUp && (
+        <Grid md={4}>
+          <Typography variant="h6" sx={{ mb: 0.5 }}>
+            Details
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Title, short description, image...
+          </Typography>
+        </Grid>
+      )}
+
+      <Grid xs={12} md={8}>
+        <Card>
+          {!mdUp && <CardHeader title="Details" />}
+
+          <Stack spacing={3} sx={{ p: 3 }}>
+            <RHFTextField name="title" label="Event Title" />
+
+            <RHFTextField name="description" label="Description" multiline rows={4} />
+
+            <Stack spacing={1.5}>
+              <Typography variant="subtitle2">Content</Typography>
+              <RHFEditor simple name="content" />
+            </Stack>
+
+            <Stack spacing={1.5}>
+              <Typography variant="subtitle2">Images</Typography>
+              <RHFUpload
+                name="image"
+                maxSize={3145728}
+                onDrop={handleDrop}
+                onDelete={handleRemoveFile}
+              />
+            </Stack>
+            <RHFTextField name="venue" label="Event Venue" />
+          </Stack>
+        </Card>
+      </Grid>
+    </>
+  );
+
+  const renderProperties = (
+    <>
+      {mdUp && (
+        <Grid md={4}>
+          <Typography variant="h6" sx={{ mb: 0.5 }}>
+            Properties
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Additional functions and attributes...
+          </Typography>
+        </Grid>
+      )}
+
+      <Grid xs={12} md={8}>
+        <Card>
+          {!mdUp && <CardHeader title="Properties" />}
+
+          <Stack spacing={3} sx={{ p: 3 }}>
+            <Box
+              columnGap={2}
+              rowGap={3}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                md: 'repeat(2, 1fr)',
+              }}
+            >
+              <RHFTextField name="code" label="Product Code" />
+
+              <RHFTextField name="sku" label="Product SKU" />
+
+              <RHFTextField
+                name="quantity"
+                label="Quantity"
+                placeholder="0"
+                type="number"
+                InputLabelProps={{ shrink: true }}
+              />
+
+              <RHFSelect native name="category" label="Category" InputLabelProps={{ shrink: true }}>
+                {PRODUCT_CATEGORY_GROUP_OPTIONS.map((category) => (
+                  <optgroup key={category.group} label={category.group}>
+                    {category.classify.map((classify) => (
+                      <option key={classify} value={classify}>
+                        {classify}
+                      </option>
+                    ))}
+                  </optgroup>
+                ))}
+              </RHFSelect>
+
+              <RHFMultiSelect
+                checkbox
+                name="colors"
+                label="Colors"
+                options={PRODUCT_COLOR_NAME_OPTIONS}
+              />
+
+              <RHFMultiSelect checkbox name="sizes" label="Sizes" options={PRODUCT_SIZE_OPTIONS} />
+            </Box>
+
+            <RHFAutocomplete
+              name="tags"
+              label="Tags"
+              placeholder="+ Tags"
+              multiple
+              freeSolo
+              options={_tags.map((option) => option)}
+              getOptionLabel={(option) => option}
+              renderOption={(props, option) => (
+                <li {...props} key={option}>
+                  {option}
+                </li>
+              )}
+              renderTags={(selected, getTagProps) =>
+                selected.map((option, index) => (
+                  <Chip
+                    {...getTagProps({ index })}
+                    key={option}
+                    label={option}
+                    size="small"
+                    color="info"
+                    variant="soft"
+                  />
+                ))
+              }
+            />
+
+            <Stack spacing={1}>
+              <Typography variant="subtitle2">Gender</Typography>
+              <RHFMultiCheckbox row name="gender" spacing={2} options={PRODUCT_GENDER_OPTIONS} />
+            </Stack>
+
+            <Divider sx={{ borderStyle: 'dashed' }} />
+
+            <Stack direction="row" alignItems="center" spacing={3}>
+              <RHFSwitch name="saleLabel.enabled" label={null} sx={{ m: 0 }} />
+              <RHFTextField
+                name="saleLabel.content"
+                label="Sale Label"
+                fullWidth
+                // disabled={!values.saleLabel.enabled}
+              />
+            </Stack>
+
+            <Stack direction="row" alignItems="center" spacing={3}>
+              <RHFSwitch name="newLabel.enabled" label={null} sx={{ m: 0 }} />
+              <RHFTextField
+                name="newLabel.content"
+                label="New Label"
+                fullWidth
+                // disabled={!values.newLabel.enabled}
+              />
+            </Stack>
+          </Stack>
+        </Card>
+      </Grid>
+    </>
+  );
+
+  const renderPricing = (
+    <>
+      {mdUp && (
+        <Grid md={4}>
+          <Typography variant="h6" sx={{ mb: 0.5 }}>
+            Pricing
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Price related inputs
+          </Typography>
+        </Grid>
+      )}
+
+      <Grid xs={12} md={8}>
+        <Card>
+          {!mdUp && <CardHeader title="Pricing" />}
+
+          <Stack spacing={3} sx={{ p: 3 }}>
+            <RHFTextField
+              name="price"
+              label="Regular Price"
+              placeholder="0.00"
+              type="number"
+              InputLabelProps={{ shrink: true }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Box component="span" sx={{ color: 'text.disabled' }}>
+                      $
+                    </Box>
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <RHFTextField
+              name="priceSale"
+              label="Sale Price"
+              placeholder="0.00"
+              type="number"
+              InputLabelProps={{ shrink: true }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Box component="span" sx={{ color: 'text.disabled' }}>
+                      $
+                    </Box>
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <FormControlLabel
+              control={<Switch checked={includeTaxes} onChange={handleChangeIncludeTaxes} />}
+              label="Price includes taxes"
+            />
+
+            {!includeTaxes && (
+              <RHFTextField
+                name="taxes"
+                label="Tax (%)"
+                placeholder="0.00"
+                type="number"
+                InputLabelProps={{ shrink: true }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Box component="span" sx={{ color: 'text.disabled' }}>
+                        %
+                      </Box>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            )}
+          </Stack>
+        </Card>
+      </Grid>
+    </>
+  );
+
+  const renderActions = (
+    <>
+      {mdUp && <Grid md={4} />}
+      <Grid xs={12} md={8} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ flexGrow: 1, pl: 3 }}>
+          <RHFSwitch name="publish" label="Publish" />
+        </Box>
+
+        <LoadingButton type="submit" variant="contained" size="large" loading={isSubmitting}>
+          {!currentProduct ? 'Create Event' : 'Save Changes'}
+        </LoadingButton>
+      </Grid>
+    </>
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={onSubmit}>
+      <Grid container spacing={3}>
+        {renderDetails}
+
+        {/* {renderProperties} */}
+
+        {/* {renderPricing} */}
+
+        {renderActions}
+      </Grid>
+    </FormProvider>
+  );
+}
+
+ProductNewEditForm.propTypes = {
+  currentProduct: PropTypes.object,
+};
---
+++ src/sections/product/view/product-create-view.jsx
@@ -0,0 +1,38 @@
+import Container from '@mui/material/Container';
+
+import { paths } from 'src/routes/paths';
+
+import { useSettingsContext } from 'src/components/settings';
+import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+
+import ProductNewEditForm from '../product-new-edit-form';
+
+// ----------------------------------------------------------------------
+
+export default function ProductCreateView() {
+  const settings = useSettingsContext();
+
+  return (
+    <Container maxWidth={settings.themeStretch ? false : 'lg'}>
+      <CustomBreadcrumbs
+        heading="Create a new Event"
+        links={[
+          {
+            name: 'Dashboard',
+            href: paths.dashboard.root,
+          },
+          {
+            name: 'Event',
+            href: paths.dashboard.event.root,
+          },
+          { name: 'New Event' },
+        ]}
+        sx={{
+          mb: { xs: 3, md: 5 },
+        }}
+      />
+
+      <ProductNewEditForm />
+    </Container>
+  );
+}
